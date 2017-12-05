0a19a28d7c81a216fa8accf8f42cdc62d62c68f7
maynard <nico.coc.accnt@gmail.com>
2017-12-05 00:10:38 -0800

    maynard2

--- Desktop/aguirre/js/init.js
+++ Desktop/aguirre/js/init.js
@@ -2,31 +2,53 @@ $(document).ready(function(){
 	$('.carousel').carousel()
 
 
-$('.travelairlineslidercontent').slick({
-  centerMode: true,
-  centerPadding: '100px',
-  slidesToShow: 3,
-  autoplay: true,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
-  ]
-});
+	$('.travelairlineslidercontent').slick({
+	  centerMode: true,
+	  centerPadding: '100px',
+	  slidesToShow: 3,
+	  autoplay: true,
+	  responsive: [
+	    {
+	      breakpoint: 768,
+	      settings: {
+	        arrows: false,
+	        centerMode: true,
+	        centerPadding: '40px',
+	        slidesToShow: 3
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        arrows: false,
+	        centerMode: true,
+	        centerPadding: '40px',
+	        slidesToShow: 1
+	      }
+	    }
+	  ]
+	});
 
+	    $(function() {
+
+        var $grid = $('.grid').masonry({
+            itemSelector: '.grid-item',
+            columnWidth: 390,
+        });
+
+        $('.material-card').materialCard();
+
+        $('.material-card').on('shown.material-card hidden.material-card', function (event) {
+            $grid.masonry();
+        });
+
+    });
+
+    var sidebar = new StickySidebar('#sidebar', {
+        containerSelector: '#main-content',
+        innerWrapperSelector: '.sidebar__inner',
+        topSpacing: 20,
+        bottomSpacing: 20
+    });
+    
 });
